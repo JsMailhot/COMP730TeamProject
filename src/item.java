4e9a262e7178cff98051e1ed90c9ac668dd6a2a3
JsMailhot <mailhotsj@gmail.com>
2021-04-14 15:22:38 -0400

    Changed type from interface to abstract class. Changed value to price. Changed qualities type from String to stats. Added toString override. Added comments.

--- src/item.java
+++ src/item.java
@@ -1,9 +1,55 @@
-public interface item {
-	int value = 0;
-	String itemName = "Default Item Name";
-	String itemDesc = "Default Item Flavor Text";
-	String qualities = "Items Stats";
-	String category = "Default";
-	
-
+/** <pre>
+ * Generic item abstract class which holds that basic requirements for an item
+ * Example of extending using this abstract class:
+ * <code>	public class newItem extends item {</code>	// declare the extension
+ * 	<code>	public newItem() {</code>	// declare constructor for class
+ * 	<code>		super();</code>	// call super method
+ * 	<code>		itemName = "item name";</code>	// define super variables
+ * 	<code>		itemVariable = new itemVariable();</code>	// define new item variables
+ * 	<code>	}</code>
+ * 	<code>}</code>
+ * </pre> */
+public abstract class item {
+	/** <pre>
+	 * The price base price for an item (doesn't account for buy / sell modifiers)
+	 * Example of defining price:
+	 * <code>	price = -1;</code>	// negative is the same as zero
+	 * 	<code>price = 1;</code>	// lowest price value
+	 * 	<code>price = 999;</code>	// highest price value
+	 * </pre> */
+	int price = 0;
+	/** <pre>
+	 * The name of the item displayed to the player
+	 * Example of defining itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>itemName = "Door";</code>	// case doesn't affect what the player sees
+	 * 	<code>out.println(itemName.toString());</code>	// because toString converts to lower case
+	 * Returns:	"door"
+	 * </pre> */
+	String itemName = "Nothing";
+	/** <pre>
+	 * The flavor text description of the item
+	 * Example of defining itemDesc:
+	 * 	<code>itemDesc = "A thing that does a thing maybe sometimes";</code>	// flavor text
+	 * </pre> */
+	String itemDesc = "Empty";
+	/** <pre>
+	 * The stats of the item
+	 * Example of defining qualities:
+	 * 	<code>qualities = new stats();</code>	// default stats
+	 * </pre> */
+	stats qualities = new stats();
+	/** <pre>
+	 * The flavor text description of the item
+	 * Example of defining category:
+	 * 	<code>category = "Weapon";</code>	// weapon category
+	 * 	<code>category = "Armor";</code>	// armor category
+	 * 	<code>category = "Misc";</code>	// misc category
+	 * </pre> */
+	String category = "All";
+	@Override
+	public String toString()
+	{
+		return itemName.toLowerCase();
+	}
 }
