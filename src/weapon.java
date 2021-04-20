bbe3d15ab917a9ccdcd615e44f970481ccb5a13c
JsMailhot <mailhotsj@gmail.com>
2021-04-19 22:03:17 -0400

    Updated weapon
    Fixed descriptions and added more constructors

--- src/weapon.java
+++ src/weapon.java
@@ -8,53 +8,231 @@
  * </pre> */
  public class weapon extends item {
 	/** <pre>
-	 * Default weapon evaluates to fists
+	 * Default weapon evaluates to nothing
 	 * Example of generating a default weapon variable to print its itemName:
 	 * <code>	import static java.lang.System.out;</code>	// import print method
 	 * 	<code>weapon default_weapon = new weapon();</code>	// simply generate a new object as a weapon variable
 	 * 	<code>out.println(default_weapon.toString());</code>	// print default_weapon
-	 * Returns:	"fists"
+	 * Returns:	"nothing"
 	 * </pre> */
 	public weapon()
 	{
 		super();
-		itemName = "fists";
 	}
 	/** <pre>
-	 * Complex weapon that evaluates to sword
-	 * Example of generating a complex weapon variable to print its itemName:
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
 	 * <code>	import static java.lang.System.out;</code>	// import print method
-	 * 	<code>weapon sword_weapon = new weapon("sword", "".toCharArray());</code>	// generate a new sword weapon
-	 * 	<code>out.println(sword_weapon.toString());</code>	// print sword_weapon
+	 * 	<code>weapon complex_weapon = new weapon("sword");</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
 	 * Returns:	"sword"
+	 * </pre> */
+	public weapon(String itemName)
+	{
+		super(itemName);
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right");</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc)
+	{
+		super(itemName, itemDesc);
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", new stats());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, stats qualities)
+	{
+		super(itemName, itemDesc, qualities);
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", "weapon");</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, String category)
+	{
+		super(itemName, itemDesc, category);
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", new stats(), "weapon");</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, stats qualities, String category)
+	{
+		super(itemName, itemDesc, qualities, category);
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", new stats(), "weapon", 0);</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, stats qualities, String category, int price)
+	{
+		super(itemName, itemDesc, qualities, category, price);
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "counters".toCharArray());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"defensive sword"
 	 * </pre> */
 	public weapon(String itemName, char... is)
 	{
-		super();
-		if(itemName == null)
+		super(itemName, is);
+		if(is == null)
 		{
 			//	what do we do when we are given nothing?
-			itemName = "fists";
-		}
-		else if(is.toString().toLowerCase() == "equip")
-		{
-			isEquip = true;
-			this.itemName = "equipt "+itemName;
-		}
-		else if(is.toString().toLowerCase() == "counters")
-		{
-			canCounter = true;
-			this.itemName = "defensive "+itemName;
-		}
-		else if(is.toString().toLowerCase() == "broken")
-		{
-			isBroken = true;
-			this.itemName = "broken "+itemName;
 		}
 		else
 		{
-			//	what do we do when we are given garbage?
-			this.itemName = itemName;
+			if(is.toString().toLowerCase().contains("counters"))
+			{
+				canCounter = true;
+				this.itemName = "defensive "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", "counters".toCharArray());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"defensive sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, char... is)
+	{
+		super(itemName, itemDesc, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("counters"))
+			{
+				canCounter = true;
+				this.itemName = "defensive "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", new stats(), "counters".toCharArray());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"defensive sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, stats qualities, char... is)
+	{
+		super(itemName, itemDesc, qualities, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("counters"))
+			{
+				canCounter = true;
+				this.itemName = "defensive "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", "weapon", "counters".toCharArray());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"defensive sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, String category, char... is)
+	{
+		super(itemName, itemDesc, category, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("counters"))
+			{
+				canCounter = true;
+				this.itemName = "defensive "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", new stats(), "weapon", "counters".toCharArray());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"defensive sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, stats qualities, String category, char... is)
+	{
+		super(itemName, itemDesc, qualities, category, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("counters"))
+			{
+				canCounter = true;
+				this.itemName = "defensive "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex weapon evaluates to sword
+	 * Example of creating a complex weapon to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>weapon complex_weapon = new weapon("sword", "just right", new stats(), "weapon", 0, "counters".toCharArray());</code>	// create a complex weapon
+	 * 	<code>out.println(complex_weapon.toString());</code>	// print complex_weapon
+	 * Returns:	"defensive sword"
+	 * </pre> */
+	public weapon(String itemName, String itemDesc, stats qualities, String category, int price, char... is)
+	{
+		super(itemName, itemDesc, qualities, category, price, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("counters"))
+			{
+				canCounter = true;
+				this.itemName = "defensive "+itemName;
+			}
 		}
 	}
 	/** <pre>
