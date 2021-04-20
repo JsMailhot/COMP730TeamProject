0338c9632066a90f1a1a1f8c0b858826bf78f089
JsMailhot <mailhotsj@gmail.com>
2021-04-19 22:04:39 -0400

    Update armor
    Fixed some constructors and comments. Added more constructors and comments.

--- src/armor.java
+++ src/armor.java
@@ -8,53 +8,231 @@
  * </pre> */
 public class armor extends item {
 	/** <pre>
-	 * Default armor evaluates to pants
+	 * Default armor evaluates to nothing
 	 * Example of creating a default armor to print its itemName:
 	 * <code>	import static java.lang.System.out;</code>	// import print method
 	 * 	<code>armor default_armor = new armor();</code>	// create a default armor
 	 * 	<code>out.println(default_armor.toString());</code>	// print default_armor
-	 * Returns:	"pants"
+	 * Returns:	"nothing"
 	 * </pre> */
 	public armor()
 	{
 		super();
-		itemName = "pants";
 	}
 	/** <pre>
-	 * Complex armor evaluates to shirt
+	 * Complex armor evaluates to pants
 	 * Example of creating a complex armor to print its itemName:
 	 * <code>	import static java.lang.System.out;</code>	// import print method
-	 * 	<code>armor complex_armor = new armor("shirt", "".toCharArray());</code>	// create a complex armor
+	 * 	<code>armor complex_armor = new armor("pants");</code>	// create a complex armor
 	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
-	 * Returns:	"shirt"
+	 * Returns:	"pants"
+	 * </pre> */
+	public armor(String itemName)
+	{
+		super(itemName);
+	}
+	/** <pre>
+	 * Complex armor evaluates to pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop");</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc)
+	{
+		super(itemName, itemDesc);
+	}
+	/** <pre>
+	 * Complex armor evaluates to pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", new stats());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, stats qualities)
+	{
+		super(itemName, itemDesc, qualities);
+	}
+	/** <pre>
+	 * Complex armor evaluates to pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", "armor");</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, String category)
+	{
+		super(itemName, itemDesc, category);
+	}
+	/** <pre>
+	 * Complex armor evaluates to pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", new stats(), "armor");</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, stats qualities, String category)
+	{
+		super(itemName, itemDesc, qualities, category);
+	}
+	/** <pre>
+	 * Complex armor evaluates to pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", new stats(), "armor", 0);</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, stats qualities, String category, int price)
+	{
+		super(itemName, itemDesc, qualities, category, price);
+	}
+	/** <pre>
+	 * Complex armor evaluates to enchanted pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "enchanted".toCharArray());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"enchanted pants"
 	 * </pre> */
 	public armor(String itemName, char... is)
 	{
-		super();
-		if(itemName == null)
+		super(itemName, is);
+		if(is == null)
 		{
 			//	what do we do when we are given nothing?
-			itemName = "pants";
-		}
-		else if(is.toString().toLowerCase() == "equip")
-		{
-			isEquip = true;
-			this.itemName = "equipt "+itemName;
-		}
-		else if(is.toString().toLowerCase() == "enchanted")
-		{
-			isEnchanted = true;
-			this.itemName = "enchanted "+itemName;
-		}
-		else if(is.toString().toLowerCase() == "broken")
-		{
-			isBroken = true;
-			this.itemName = "broken "+itemName;
 		}
 		else
 		{
-			//	what do we do when we are given garbage?
-			this.itemName = itemName;
+			if(is.toString().toLowerCase().contains("enchanted"))
+			{
+				isEnchanted = true;
+				this.itemName = "enchanted "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex armor evaluates to enchanted pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", "enchanted".toCharArray());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"enchanted pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, char... is)
+	{
+		super(itemName, itemDesc, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("enchanted"))
+			{
+				isEnchanted = true;
+				this.itemName = "enchanted "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex armor evaluates to enchanted pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", new stats(), "enchanted".toCharArray());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"enchanted pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, stats qualities, char... is)
+	{
+		super(itemName, itemDesc, qualities, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("enchanted"))
+			{
+				isEnchanted = true;
+				this.itemName = "enchanted "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex armor evaluates to enchanted pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", "armor", "enchanted".toCharArray());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"enchanted pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, String category, char... is)
+	{
+		super(itemName, itemDesc, category, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("enchanted"))
+			{
+				isEnchanted = true;
+				this.itemName = "enchanted "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex armor evaluates to enchanted pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", new stats(), "enchanted".toCharArray());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"enchanted pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, stats qualities, String category, char... is)
+	{
+		super(itemName, itemDesc, qualities, category, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("enchanted"))
+			{
+				isEnchanted = true;
+				this.itemName = "enchanted "+itemName;
+			}
+		}
+	}
+	/** <pre>
+	 * Complex armor evaluates to enchanted pants
+	 * Example of creating a complex armor to print its itemName:
+	 * <code>	import static java.lang.System.out;</code>	// import print method
+	 * 	<code>armor complex_armor = new armor("pants", "required to shop", new stats(), 0, "enchanted".toCharArray());</code>	// create a complex armor
+	 * 	<code>out.println(complex_armor.toString());</code>	// print complex_armor
+	 * Returns:	"enchanted pants"
+	 * </pre> */
+	public armor(String itemName, String itemDesc, stats qualities, String category, int price, char... is)
+	{
+		super(itemName, itemDesc, qualities, category, price, is);
+		if(is == null)
+		{
+			//	what do we do when we are given nothing?
+		}
+		else
+		{
+			if(is.toString().toLowerCase().contains("enchanted"))
+			{
+				isEnchanted = true;
+				this.itemName = "enchanted "+itemName;
+			}
 		}
 	}
 	/** <pre>
